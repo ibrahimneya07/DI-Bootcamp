95d5a65c6e803af0b445d522b31453b432860628
ibrahimneya07 <111282420+ibrahimneya07@users.noreply.github.com>
2022-12-12 11:46:35 +0000

    Add files via upload

---
+++ Week-8/exo3/Gifs/mygifs/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from datetime import datetime
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'Category {self.name}'
+
+class Gif(models.Model):
+    title = models.CharField(max_length=100)
+    URL = models.URLField()
+    uploader_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default = datetime.today())
+    category = models.ManyToManyField(Category, related_name='categories', blank=True)
+
+    def __str__(self):
+           return f'{self.title}'
